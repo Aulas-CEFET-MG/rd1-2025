b36f4236192080c67c94fd0a5921e5ad6eeb60b8
Danilo B S <daniloboechat@msn.com>
2025-09-17 18:12:07 -0300

    Update 030925-Dollynho.c
    add menu completo dollynho

--- funcaoTadMenu/030925-Dollynho.c
+++ funcaoTadMenu/030925-Dollynho.c
@@ -10,20 +10,26 @@ typedef struct
     char raridade;
 } Dollynho;
 
-Dollynho registrarDolinho(){
-
+Dollynho registrarDolinho()
+{
+    Dollynho dolly;
     char nome[60];
     char sabor[60];
-    int dano;
-    int vida;
+    int dano, vida;
     char raridade;
-    Dollynho dolly;
+    int c;
+
+    // Limpa o buffer antes de fgets (caso venha de um scanf anterior)
+    while ((c = getchar()) != '\n' && c != EOF)
+        ;
 
     printf("Digite o nome do seu amiguinho: \n");
     fgets(nome, sizeof(nome), stdin);
+    nome[strcspn(nome, "\n")] = '\0'; // Remove o \n
 
     printf("Digite o sabor do seu dollynho:\n");
     fgets(sabor, sizeof(sabor), stdin);
+    sabor[strcspn(sabor, "\n")] = '\0'; // Remove o \n
 
     printf("Digite o nível de Dano do seu dollynho:\n");
     scanf("%d", &dano);
@@ -31,52 +37,162 @@ Dollynho registrarDolinho(){
     printf("Digite o nível de Vida do seu dollynho:\n");
     scanf("%d", &vida);
 
-    // --- AQUI ESTÁ A SOLUÇÃO ---
-    // Limpa o buffer de entrada removendo o '\n' e quaisquer outros caracteres restantes
-    int c;
+    // Limpa o buffer antes de ler o caractere
     while ((c = getchar()) != '\n' && c != EOF)
         ;
-    // ----------------------------
 
     printf("Digite o nível (A-Z) de Raridade do seu dollynho:\n");
     scanf("%c", &raridade);
 
-    dolly.dano = dano;
-    dolly.raridade = raridade;
-    strcpy(dolly.sabor, sabor);
+    // Preenche a struct
     strcpy(dolly.nome, nome);
+    strcpy(dolly.sabor, sabor);
+    dolly.dano = dano;
     dolly.vida = vida;
-
-    while ((c = getchar()) != '\n' && c != EOF)
-        ;
+    dolly.raridade = raridade;
 
     return dolly;
 }
 
-void listarDollynhos(Dollynho amiguinhos[], int numeroDollynhos){
+void listarDollynhos(Dollynho amiguinhos[], int numeroDollynhos)
+{
 
-    for(int i = 0; i < numeroDollynhos; i++){
+    for (int i = 0; i < numeroDollynhos; i++)
+    {
         printf("Nome: %s \t | Vida: %d\n", amiguinhos[i].nome, amiguinhos[i].vida);
         printf("Sabor: %s \t | Dano: %d\n", amiguinhos[i].sabor, amiguinhos[i].dano);
-        printf("Raridade: %c\n", amiguinhos[i].raridade);
+        printf("Raridade: %c\n\n", amiguinhos[i].raridade);
     }
 }
 
-int main(){
+void exibirMenuDollynho()
+{
+    printf("\n===================== MENU =====================\n");
+    printf("= 1 - Registrar Dollynho                       =\n");
+    printf("= 2 - Atualizar Nivel de Vida                  =\n");
+    printf("= 3 - Listar Dollynhos                         =\n");
+    printf("= 4 - Exibir Ranking de Dollynhos por Raridade =\n");
+    printf("= 5 - Sair                                     =\n");
+    printf("================================================\n");
+    printf("Escolha uma opcao: ");
+}
 
-    int MAX_DOLLYNHOS = 2;
-    Dollynho dollynhos[MAX_DOLLYNHOS];
+Dollynho *inicializarVetorDollynhos(int totalDollynhos)
+{
+
+    Dollynho *vetor = malloc(totalDollynhos * sizeof(Dollynho));
+    if (vetor == NULL)
+    {
+        printf("Erro ao alocar memoria!");
+        return NULL;
+    }
+
+    for (int i = 0; i < totalDollynhos; i++)
+    {
+        vetor[i].dano = 0;
+        strcpy(vetor[i].nome, "");
+        vetor[i].raridade = 'O';
+        strcpy(vetor[i].sabor, "");
+        vetor[i].vida = 0;
+    }
+
+    return vetor;
+}
+
+void atualizarVidaDollynho(Dollynho dollynhos[], int totalDollynhos)
+{
+
+    char nome[50];
+
+    int c;
+    // Limpa o buffer antes de fgets (caso venha de um scanf anterior)
+    while ((c = getchar()) != '\n' && c != EOF)
+        ;
+
+    printf("Qual Dollynho (seu amiguinho) deseja atualizar o nivel de vida?");
+    fgets(nome, sizeof(nome), stdin);
+    nome[strcspn(nome, "\n")] = '\0'; // Remove o \n
+
+    for (int i = 0; i < totalDollynhos; i++)
+    {
+        if (strcmp(dollynhos[i].nome, nome) == 0)
+        {
+            printf("Qual é o novo nivel de Vida do %s?", nome);
+            scanf("%d", &dollynhos[i].vida);
+        }
+    }
+}
+
+void listarDollynhosPorRaridade(Dollynho dollynhos[], int totalDollynhos)
+{
+
+    for (int i = 0; i < totalDollynhos - 1; i++)
+    {
+        for (int j = i + 1; j < totalDollynhos; j++)
+        {
+            if (dollynhos[j].raridade > dollynhos[i].raridade)
+            {
+                Dollynho temp = dollynhos[i];
+                dollynhos[i] = dollynhos[j];
+                dollynhos[j] = temp;
+            }
+        }
+    }
+
+    printf("================ Ranking por Raridade ================");
+    for (int i = 0; i < totalDollynhos; i++)
+    {
+        printf("%dº - %s | Raridade: %c | Vida: %d | Dano: %d | Sabor: %s\n\n",
+               i + 1,
+               dollynhos[i].nome,
+               dollynhos[i].raridade,
+               dollynhos[i].vida,
+               dollynhos[i].dano,
+               dollynhos[i].sabor);
+    }
+    printf("======================================================\n\n");
+}
+
+int main()
+{
+
+    int MAX_DOLLYNHOS;
+    printf("Quantos Dollynhos (seus amiguinhos) voce deseja cadastrar? ");
+    scanf("%d", &MAX_DOLLYNHOS);
+
+    Dollynho *dollynhos = inicializarVetorDollynhos(MAX_DOLLYNHOS);
     int dollynhosCadastrados = 0;
 
-    dollynhos[dollynhosCadastrados] = registrarDolinho();
-    dollynhosCadastrados++;
+    int opcao = 0;
 
-    listarDollynhos(dollynhos, dollynhosCadastrados);
+    while (opcao != 5)
+    {
 
-    dollynhos[dollynhosCadastrados] = registrarDolinho();
-    dollynhosCadastrados++;
+        exibirMenuDollynho();
+        scanf("%d", &opcao);
 
-    listarDollynhos(dollynhos, dollynhosCadastrados);
+        switch (opcao)
+        {
+        case 1:
+            dollynhos[dollynhosCadastrados] = registrarDolinho();
+            dollynhosCadastrados++;
+            break;
+        case 2:
+            atualizarVidaDollynho(dollynhos, dollynhosCadastrados);
+            break;
+        case 3:
+            listarDollynhos(dollynhos, dollynhosCadastrados);
+            break;
+        case 4:
+            listarDollynhosPorRaridade(dollynhos, dollynhosCadastrados);
+            break;
+        case 5:
+            printf("See you soon :)");
+            break;
 
+        default:
+            break;
+        }
+    }
 
 }
